3ed09f924c20faf103f7d384fab65a802b06db3a
githubfoam <lequestionis@gmail.com>
2020-10-16 12:28:45 +0300

    vagrantcloud

--- scripts/deploy-debian-buster.sh
+++ scripts/deploy-debian-buster.sh
@@ -13,34 +13,7 @@ vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with differ
 vagrant box add "debian/buster64" --provider=libvirt
 vagrant init --template Vagrantfile.provision.bash.erb
 # must be created in project root directory with Vagrantfile template file
-vagrant up --provider=libvirt "vg-debian-01"
-
-# https://app.vagrantup.com/generic/boxes/netbsd8
-# vagrant box add "generic/netbsd8" --provider=virtualbox
-# vagrant init --template Vagrantfile.provision.bash.netbsd.erb
-# vagrant up --provider=libvirt "vg-netbsd-04"
-
-# https://app.vagrantup.com/dragonflybsd/boxes/dragonfly520
-# vagrant box add "generic/alpine39" --provider=virtualbox
-# vagrant init --template Vagrantfile.provision.bash.generic.erb
-# vagrant up --provider=libvirt "vg-alpine-01"
-# vagrant destroy -f "vg-netbsd-03"
-# rm Vagrantfile
-
-# https://app.vagrantup.com/NetBSD/boxes/NetBSD-8.0
-# vagrant box add "NetBSD/NetBSD-7.0" --provider=virtualbox
-# vagrant mutate "NetBSD/NetBSD-7.0" libvirt
-# vagrant init --template Vagrantfile.provision.bash.netbsd.erb
-# vagrant up --provider=libvirt "vg-netbsd-01"
-
-# https://app.vagrantup.com/NetBSD/boxes/NetBSD-8.0
-# vagrant box add "NetBSD/NetBSD-8.0" --provider=virtualbox
-# vagrant mutate "NetBSD/NetBSD-8.0" libvirt
-# vagrant init --template Vagrantfile.provision.bash.netbsd.erb
-# vagrant up --provider=libvirt "vg-netbsd-02"
-# vagrant destroy -f "vg-netbsd-02"
-# rm Vagrantfile
-# vagrant ssh vgnode04 -c "hostnamectl"
+vagrant up --provider=libvirt "vg-debian-05"
 
 vagrant box list #veridy installed boxes
 #A Vagrant environment or target machine is required to run this
